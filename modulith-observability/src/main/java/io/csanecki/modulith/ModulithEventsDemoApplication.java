b6ae5dbe450f2926cf2aeeaedbff0072111e0a8d
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-07-03 15:53:31 +0200

    cleaned code

--- modulith-observability/src/main/java/io/csanecki/modulith/ModulithEventsDemoApplication.java
+++ modulith-observability/src/main/java/io/csanecki/modulith/ModulithEventsDemoApplication.java
@@ -1,25 +1,13 @@
 package io.csanecki.modulith;
 
-import io.csanecki.modulith.secondmodule.Second;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ModulithEventsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModulithEventsDemoApplication.class, args);
-    }
-
-    @Autowired
-    Second second;
-
-    @Bean
-    CommandLineRunner init() {
-        return args -> second.doSomething();
     }
 
 }
