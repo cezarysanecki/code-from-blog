9ee95e5c72cf5ec3f0f38337f9f6e1ae926ef599
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-07-05 17:26:11 +0200

    initial commit for draft cqrs

---
+++ draft-cqrs/src/main/java/io/csanecki/draft/DraftCqrsApplication.java
@@ -0,0 +1,13 @@
+package io.csanecki.draft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftCqrsApplication.class, args);
+	}
+
+}
