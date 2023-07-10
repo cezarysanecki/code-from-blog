9fb0245c99e8973de44786108176db3100eca3f3
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-07-10 08:39:44 +0200

    prepared land transport static fields

---
+++ draft-cqrs/src/main/java/io/csanecki/cqrs/landtransport/LandTransportFields.java
@@ -0,0 +1,11 @@
+package io.csanecki.cqrs.landtransport;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class LandTransportFields {
+
+  final static String FORM_OF_TRANSPORT = "formOfTransport";
+
+}
