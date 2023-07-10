e592d0e1a7b96f833310a00fc4a48bbfc1ee6554
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-07-10 15:08:45 +0200

    removed method

--- draft-cqrs/src/main/java/io/csanecki/cqrs/draft/api/DraftValidationException.java
+++ draft-cqrs/src/main/java/io/csanecki/cqrs/draft/api/DraftValidationException.java
@@ -4,33 +4,17 @@ import io.csanecki.cqrs.error.api.Error;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
-
-import java.util.Collection;
-import java.util.Set;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @EqualsAndHashCode(callSuper = false)
+@RequiredArgsConstructor
 public class DraftValidationException extends RuntimeException {
 
   @NonNull
   private final DraftId draftId;
 
   @NonNull
-  private final Collection<Error> errors;
-
-  public DraftValidationException(
-      @NonNull DraftId draftId,
-      @NonNull Error error
-  ) {
-    this(draftId, Set.of(error));
-  }
-
-  public DraftValidationException(
-      @NonNull DraftId draftId,
-      @NonNull Collection<Error> errors
-  ) {
-    this.draftId = draftId;
-    this.errors = errors;
-  }
+  private final Error errors;
 
 }
