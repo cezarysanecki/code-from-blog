8d26b19bd97f2a4b2c6101d8c5f6b03654fb10ed
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-07-10 11:36:30 +0200

    prepared facade

---
+++ draft-cqrs/src/main/java/io/csanecki/cqrs/landtransport/LandTransportFacade.java
@@ -0,0 +1,27 @@
+package io.csanecki.cqrs.landtransport;
+
+import io.csanecki.cqrs.draft.DraftId;
+import io.csanecki.cqrs.landtransport.command.UpdateFormOfTransportCommand;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public class LandTransportFacade {
+
+  private final LandTransportRepository landTransportRepository;
+  private final CommandValidator commandValidator;
+
+  @Transactional
+  public void updateFormOfTransport(
+      @NonNull DraftId draftId,
+      @NonNull UpdateFormOfTransportCommand command
+  ) {
+    commandValidator.validate(draftId, command);
+
+    LandTransport landTransport = landTransportRepository.findByDraftIdForce(draftId);
+    landTransport.assignFormOfTransport(command.formOfTransport());
+  }
+
+}
